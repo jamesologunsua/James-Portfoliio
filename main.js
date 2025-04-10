e871825dc7a1e8135bc43c40bef1ed5bbe591669
James Ologunsua <jamesologunsua@gmail.com>
2025-04-10 08:58:27 +0100

    Add alert button and cv file

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents")
 
 
+document.getElementById('cv-btn').addEventListener('click', function() {
+    alert("You have download the CV");
+});
+
 function opentab(tabname) {
 for(tablink of tablinks) {
 tablink.classList.remove("active-link");
@@ -27,7 +31,24 @@ function closemenu() {
     sidemenu.style.right = "-200px";
 }
 
-
+// Add click handler for download button
+document.querySelector('.btn2').addEventListener('click', function(e) {
+    // Optional: Add download tracking
+    console.log('CV downloaded at:', new Date().toLocaleString());
+    
+    // For Cordova apps, add this:
+    if(typeof cordova !== 'undefined') {
+        e.preventDefault();
+        const fileTransfer = new FileTransfer();
+        const uri = encodeURI(this.href);
+        fileTransfer.download(
+            uri,
+            cordova.file.externalRootDirectory + 'James_CV.pdf',
+            () => alert('CV downloaded successfully!'),
+            (error) => alert('Error: ' + error.source)
+        );
+    }
+});
 
 
 
